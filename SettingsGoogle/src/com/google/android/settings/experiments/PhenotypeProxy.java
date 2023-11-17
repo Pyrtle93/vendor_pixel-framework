cb14f129d69e6ce443d5e6b4234ae8ba6a87a64f
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-18 00:18:29 +0800

    SettingsGoogle: Update fuelgauge dependencies
    Change-Id: I4f97f71ca4b7576f89c630086d5eec9e5c98e1bb
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- SettingsGoogle/src/com/google/android/settings/experiments/PhenotypeProxy.java
+++ SettingsGoogle/src/com/google/android/settings/experiments/PhenotypeProxy.java
@@ -1,76 +1,48 @@
 package com.google.android.settings.experiments;
 
 import android.content.ContentProviderClient;
-import android.util.Log;
-import android.os.Bundle;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import java.util.List;
 
-public class PhenotypeProxy
-{
-    private static final Uri PROXY_AUTHORITY;
-    
-    static {
-        PROXY_AUTHORITY = new Uri.Builder().scheme("content").authority("com.google.android.settings.intelligence.provider.experimentflags").build();
+/* loaded from: classes2.dex */
+public class PhenotypeProxy {
+    private static final Uri PROXY_AUTHORITY = new Uri.Builder().scheme("content").authority("com.google.android.settings.intelligence.provider.experimentflags").build();
+
+    public static boolean getBooleanFlagByPackageAndKey(Context context, String str, String str2, boolean z) {
+        Bundle flagByPackageAndKey = getFlagByPackageAndKey(context, str, str2, "getBooleanForPackageAndKey");
+        return flagByPackageAndKey == null ? z : flagByPackageAndKey.getBoolean("value", z);
     }
 
-    public static boolean getFlagByPackageAndKey(Context context, String call, String s, boolean z) {
-        Bundle bundle = new Bundle();
-        bundle.putString("package_name", call);
-        bundle.putString("key", s);
-        Bundle bundle2 = null;
-        try {
-            ContentProviderClient acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
-            bundle2 = acquireUnstableContentProviderClient.call("getBooleanForPackageAndKey", null, bundle);
-            acquireUnstableContentProviderClient.close();
-        } catch (Exception ex) {
-            Log.e("PhenotypeProxy", "Failed to query experiment provider", ex);
-        } catch (Throwable th) {
-            th.addSuppressed(th);
-        }
-        if (bundle2 == null) {
-            return z;
-        }
-        return bundle2.getBoolean("value", z);
+    public static double getDoubleFlagByPackageAndKey(Context context, String str, String str2, double d) {
+        Bundle flagByPackageAndKey = getFlagByPackageAndKey(context, str, str2, "getDoubleForPackageAndKey");
+        return flagByPackageAndKey == null ? d : flagByPackageAndKey.getDouble("value", d);
     }
 
-    public static boolean getBooleanFlagByPackageAndKey(Context context, String call, String s, boolean b) {
-        Bundle bundle = new Bundle();
-        bundle.putString("package_name", call);
-        bundle.putString("key", s);
-        Bundle bundle2 = null;
-        try {
-            ContentProviderClient acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
-            bundle2 = acquireUnstableContentProviderClient.call("getBooleanForPackageAndKey", null, bundle);
-            acquireUnstableContentProviderClient.close();
-        } catch (Exception ex) {
-            Log.e("PhenotypeProxy", "Failed to query experiment provider", ex);
-        } catch (Throwable th) {
-            th.addSuppressed(th);
-        }
-        if (bundle2 == null) {
-            return b;
-        }
-        return bundle2.getBoolean("value", b);
+    public static String getStringFlagByPackageAndKey(Context context, String str, String str2, String str3) {
+        Bundle flagByPackageAndKey = getFlagByPackageAndKey(context, str, str2, "getStringForPackageAndKey");
+        return flagByPackageAndKey == null ? str3 : flagByPackageAndKey.getString("value", str3);
     }
 
-    public static String getStringFlagByPackageAndKey(Context context, String call, String s, String s2) {
+    public static List<String> getStringListFlagByPackageAndKey(Context context, String str, String str2, List<String> list) {
+        Bundle flagByPackageAndKey = getFlagByPackageAndKey(context, str, str2, "getStringListForPackageAndKey");
+        return flagByPackageAndKey == null ? list : flagByPackageAndKey.getStringArrayList("value");
+    }
+
+    private static Bundle getFlagByPackageAndKey(Context context, String str, String str2, String str3) {
         Bundle bundle = new Bundle();
-        bundle.putString("package_name", call);
-        bundle.putString("key", s);
+        bundle.putString("package_name", str);
+        bundle.putString("key", str2);
         Bundle bundle2 = null;
         try {
-            ContentProviderClient acquireUnstableContentProviderClient = context.getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
-            bundle2 = acquireUnstableContentProviderClient.call("getStringForPackageAndKey", null, bundle);
+            ContentProviderClient acquireUnstableContentProviderClient = context.getApplicationContext().getContentResolver().acquireUnstableContentProviderClient(PROXY_AUTHORITY);
+            bundle2 = acquireUnstableContentProviderClient.call(str3, null, bundle);
             acquireUnstableContentProviderClient.close();
-        } catch (Exception ex) {
-            Log.e("PhenotypeProxy", "Failed to query experiment provider", ex);
-        } catch (Throwable th) {
-            th.addSuppressed(th);
+        } catch (Exception e) {
+            Log.e("PhenotypeProxy", "Failed to query experiment provider", e);
         }
-        if (bundle2 == null) {
-            return s2;
-        }
-        return bundle2.getString("value", s2);
+        return bundle2;
     }
 }
--- SettingsGoogle/src/com/google/android/settings/fuelgauge/BatteryUsageContentProvider.java
+++ SettingsGoogle/src/com/google/android/settings/fuelgauge/BatteryUsageContentProvider.java
@@ -7,54 +7,71 @@ import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
 import android.util.Log;
+import com.android.internal.os.PowerProfile;
 import com.android.settings.fuelgauge.batteryusage.BatteryChartPreferenceController;
 import com.android.settings.fuelgauge.batteryusage.BatteryDiffEntry;
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.function.Consumer;
 
+/* loaded from: classes2.dex */
 public final class BatteryUsageContentProvider extends ContentProvider {
     private static final UriMatcher URI_MATCHER;
     static List<BatteryDiffEntry> sCacheBatteryDiffEntries;
 
-    @Override
+    /* renamed from: $r8$lambda$piQTleXoYTG2WU3nGzKMy-cQQZQ */
+    public static /* synthetic */ void m4598$r8$lambda$piQTleXoYTG2WU3nGzKMycQQZQ(MatrixCursor matrixCursor, BatteryDiffEntry batteryDiffEntry) {
+        lambda$getBatteryUsageData$0(matrixCursor, batteryDiffEntry);
+    }
+
+    @Override // android.content.ContentProvider
     public String getType(Uri uri) {
         return null;
     }
 
     static {
-        URI_MATCHER = new UriMatcher(-1);
-        URI_MATCHER.addURI("com.google.android.settings.fuelgauge.provider", "BatteryUsageState", 1);
+        UriMatcher uriMatcher = new UriMatcher(-1);
+        URI_MATCHER = uriMatcher;
+        uriMatcher.addURI("com.google.android.settings.fuelgauge.provider", "BatteryUsageState", 1);
         sCacheBatteryDiffEntries = null;
     }
 
-    @Override
+    @Override // android.content.ContentProvider
     public boolean onCreate() {
         Log.v("BatteryUsageContentProvider", "initialize provider");
         return true;
     }
 
-    @Override
+    @Override // android.content.ContentProvider
     public Uri insert(Uri uri, ContentValues contentValues) {
         throw new UnsupportedOperationException("insert() unsupported!");
     }
 
-    @Override
+    @Override // android.content.ContentProvider
     public int update(Uri uri, ContentValues contentValues, String str, String[] strArr) {
         throw new UnsupportedOperationException("update() unsupported!");
     }
 
-    @Override
+    @Override // android.content.ContentProvider
     public int delete(Uri uri, String str, String[] strArr) {
         throw new UnsupportedOperationException("delete() unsupported!");
     }
 
-    @Override
+    @Override // android.content.ContentProvider
     public Cursor query(Uri uri, String[] strArr, String str, String[] strArr2, String str2) {
         Log.d("BatteryUsageContentProvider", "query:" + uri);
         if (URI_MATCHER.match(uri) != 1) {
             return null;
         }
         return getBatteryUsageData();
+    }
+
+    @Override // android.content.ContentProvider
+    public void dump(FileDescriptor fileDescriptor, PrintWriter printWriter, String[] strArr) {
+        printWriter.println("dump BatteryUsage states:");
+        double averagePowerForOrdinal = new PowerProfile(getContext()).getAveragePowerForOrdinal("screen.full.display", 0);
+        printWriter.println("\tPowerProfile.getAveragePowerForOrdinal(): " + averagePowerForOrdinal);
     }
 
     private Cursor getBatteryUsageData() {
@@ -67,18 +84,21 @@ public final class BatteryUsageContentProvider extends ContentProvider {
             return null;
         }
         final MatrixCursor matrixCursor = new MatrixCursor(BatteryUsageContract.KEYS_BATTERY_USAGE_STATE);
-        list.forEach(new Consumer() {
-            @Override
+        list.forEach(new Consumer() { // from class: com.google.android.settings.fuelgauge.BatteryUsageContentProvider$$ExternalSyntheticLambda0
+            @Override // java.util.function.Consumer
             public final void accept(Object obj) {
-                BatteryDiffEntry batteryDiffEntry = (BatteryDiffEntry) obj;
-                if (batteryDiffEntry.mBatteryHistEntry == null || batteryDiffEntry.getPercentOfTotal() == 0.0d) {
-                    return;
-                }
-                addUsageDataRow(matrixCursor, batteryDiffEntry);
+                BatteryUsageContentProvider.m4598$r8$lambda$piQTleXoYTG2WU3nGzKMycQQZQ(matrixCursor, (BatteryDiffEntry) obj);
             }
         });
         Log.d("BatteryUsageContentProvider", "usage data count:" + matrixCursor.getCount());
         return matrixCursor;
+    }
+
+    public static /* synthetic */ void lambda$getBatteryUsageData$0(MatrixCursor matrixCursor, BatteryDiffEntry batteryDiffEntry) {
+        if (batteryDiffEntry.mBatteryHistEntry == null || batteryDiffEntry.getPercentage() == 0.0d) {
+            return;
+        }
+        addUsageDataRow(matrixCursor, batteryDiffEntry);
     }
 
     private static void addUsageDataRow(MatrixCursor matrixCursor, BatteryDiffEntry batteryDiffEntry) {
@@ -87,6 +107,6 @@ public final class BatteryUsageContentProvider extends ContentProvider {
             Log.w("BatteryUsageContentProvider", "no package name found for\n" + batteryDiffEntry);
             return;
         }
-        matrixCursor.addRow(new Object[]{new Long(batteryDiffEntry.mBatteryHistEntry.mUserId), packageName, new Double(batteryDiffEntry.getPercentOfTotal()), new Long(batteryDiffEntry.mForegroundUsageTimeInMs), new Long(batteryDiffEntry.mBackgroundUsageTimeInMs)});
+        matrixCursor.addRow(new Object[]{new Long(batteryDiffEntry.mBatteryHistEntry.mUserId), packageName, new Double(batteryDiffEntry.getPercentage()), new Long(batteryDiffEntry.mForegroundUsageTimeInMs), new Long(batteryDiffEntry.mBackgroundUsageTimeInMs)});
     }
 }
