2d4bdde25e08e68616842a47e01e59cf6840b1f7
someone5678 <someone5678@users.noreply.github.com>
2023-12-28 22:48:57 +0900

    pixel-framework: Move in TARGET_INCLUDE_PIXEL_FRAMEWORKS
    Change-Id: I26a732fdb089912e47215db8251e13357ee101dc

--- config.mk
+++ config.mk
@@ -1,3 +1,7 @@
+# Inherit from pixel-framework config
+TARGET_INCLUDE_PIXEL_FRAMEWORKS ?= true
+ifeq ($(TARGET_INCLUDE_PIXEL_FRAMEWORKS),true)
 PRODUCT_PACKAGES += \
     SystemUIGoogle \
     SettingsGoogle
+endif
