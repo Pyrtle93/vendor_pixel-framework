16b28ddeaf85a6483839cfd9876e2459287b6f03
Pyrtle93 <xxpyrtlexx93@gmail.com>
2024-02-01 00:40:32 +0530

    SystemUiGoogle: Drop ThemeOverlayControllerGoogle

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleCoreStartableModule.kt
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleCoreStartableModule.kt
@@ -63,7 +63,6 @@ import com.android.systemui.volume.VolumeUI
 import com.android.systemui.wallpapers.dagger.WallpaperModule
 import com.android.systemui.wmshell.WMShell
 
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle
 import com.google.android.systemui.GoogleServices
 import com.google.android.systemui.smartspace.KeyguardSmartspaceStartable
 
@@ -206,7 +205,7 @@ abstract class SystemUIGoogleCoreStartableModule {
     @Binds
     @IntoMap
     @ClassKey(ThemeOverlayController::class)
-    abstract fun bindThemeOverlayControllerGoogle(sysui: ThemeOverlayControllerGoogle): CoreStartable
+    abstract fun bindThemeOverlayController(sysui: ThemeOverlayController): CoreStartable
 
     /** Inject into ToastUI.  */
     @Binds
